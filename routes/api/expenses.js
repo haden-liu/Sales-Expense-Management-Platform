7fb3b289edd3584524394f77d54981c079035949
Haden Liu <haden.liu30@gmail.com>
2020-07-23 18:43:33 +1000

    second commit

---
+++ routes/api/expenses.js
@@ -0,0 +1,74 @@
+const router = require("express").Router();
+let Expense = require("../../models/expenseModel")
+
+router.route("/").get((req, res) => {
+    Expense.find()
+      .then(expenses => res.json(expenses))
+      .catch(err => {
+        console.log(err)
+        res.status(500).json({err})});
+  });
+  
+  router.route("/add").post((req, res) => {
+    console.log(req.body)
+    const title = req.body.title;
+    const date = Date.parse(req.body.date);
+    const category = req.body.category;
+    const clienttocharge = req.body.clienttocharge;
+    const amount = Number(req.body.amount);
+    const notes = req.body.notes;
+    const creator = req.body.creator;
+  
+    const newExpense = new Expense({ 
+      title,
+      date,
+      category,
+      clienttocharge, 
+      amount, 
+      notes,
+      creator
+    });
+  
+    newExpense
+      .save()
+      .then(() => res.json("Expense added!"))
+      .catch(err => res.status(500).json("Error: " + err));
+  });
+
+  router.route('/:id').get((req, res) => {
+    console.log(req.params)
+    Expense.findById(req.params.id)
+    .then(expense => res.json(expense))
+    .catch(err => res.status(500).json("Error:" + err))
+  });
+
+  router.route('/:id').delete ((req, res) => {
+    Expense.findByIdAndDelete(req.params.id)
+    .then(() => res.json("Expense Deleted"))
+    .catch(err => res.status(500).json("Error:" + err))
+  });
+  
+  router.route('/update/:id').post ((req, res) => {
+    Expense.findById(req.params.id)
+    .then(expense => {
+
+      expense.title = req.body.title,
+      expense.date = Date.parse(req.body.date);
+      expense.category = req.body.category;
+      expense.clienttocharge = req.body.clienttocharge;
+      expense.amount = Number (req.body.amount);
+      expense.notes = req.body.notes
+    
+
+      expense.save()
+     .then(() => res.json("Expense updated")) 
+     .catch(err => res.status(500).json("Error:" + err));
+    
+    })
+    
+    .catch(err => res.status(500).json("Error:" + err));
+
+      
+  });
+
+module.exports = router;
---
+++ routes/api/user.js
@@ -0,0 +1,136 @@
+const router = require("express").Router();
+const config = require("config");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+let User = require("../../models/userModel");
+const auth = require("../../middleware/auth");
+
+router.route("/").get((req, res) => {
+  User.find()
+    .then(users => res.json(users))
+    .catch(err => res.status(500).json("Error: " + err));
+});
+
+router.route("/signup").post((req, res) => {
+  const { body } = req;
+  let { firstname, lastname, email, password } = body;
+
+  if (!firstname) {
+    return res.send({
+      success: false,
+      message: "Error: First name cannot be blank"
+    });
+  }
+  if (!lastname) {
+    return res.send({
+      success: false,
+      message: "Error: Last name cannot be blank"
+    });
+  }
+  if (!email) {
+    return res.send({
+      success: false,
+      message: "Error: Email cannot be blank"
+    });
+  }
+  if (!password) {
+    return res.send({
+      success: false,
+      message: "Error: Password cannot be blank"
+    });
+  }
+
+  email = email.toLowerCase();
+
+  User.find(
+    {
+      email: email
+    },
+    (err, previousUsers) => {
+      if (err) {
+        return res.send({
+          success: false,
+          message: "Error: Server error"
+        });
+      } else if (previousUsers.length > 0) {
+        return res.send({
+          success: false,
+          message: "Error: Email address is already in use."
+        });
+      }
+
+      const newUser = new User();
+
+      newUser.firstname = firstname;
+      newUser.lastname = lastname;
+      newUser.email = email;
+      newUser.password = newUser.generateHash(password);
+      newUser.save().then(user => {
+        jwt.sign(
+          { id: user.id },
+          config.get("jwtSecret"),
+          { expiresIn: 3600 },
+          (err, token) => {
+            if (err) throw err;
+            res.json({
+              token,
+              user: {
+                id: user.id,
+                firstname: user.firstname,
+                email: user.email
+              }
+            });
+          }
+        );
+      });
+    }
+  );
+});
+
+router.route("/login", auth).post((req, res) => {
+  const { body } = req;
+  let { email, password } = body;
+
+  if (!email) {
+    return res.send({
+      success: false,
+      message: "Error: Email cannot be blank"
+    });
+  }
+  if (!password) {
+    return res.send({
+      success: false,
+      message: "Error: Password cannot be blank"
+    });
+  }
+
+  email = email.toLowerCase();
+
+  User.findOne({ email }).then(user => {
+    if (!user) return res.status(500).json({ msg: "User does not exist" });
+
+    // validate password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (!isMatch) return res.status(500).json({ msg: "Invalid credentials" });
+
+      jwt.sign(
+        { id: user.id },
+        config.get("jwtSecret"),
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({
+            token,
+            user: {
+              id: user.id,
+              firstname: user.firstname,
+              email: user.email
+            }
+          });
+        }
+      );
+    });
+  });
+});
+
+module.exports = router;
