fe53a3f66c852b5ecb652ba7dcc4948afc654886
Haden Liu <haden.liu30@gmail.com>
2020-08-09 23:35:12 +1000

    heroku

---
+++ client/build/precache-manifest.e6c98fdb12fc7eb7dd27c3f21b13456c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ff789ae2fd192ff471112693bf101c5",
+    "url": "/haden-liu/index.html"
+  },
+  {
+    "revision": "eb1212a9a531504a1b43",
+    "url": "/haden-liu/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "2e8161356437cf74f058",
+    "url": "/haden-liu/static/css/main.81f9f151.chunk.css"
+  },
+  {
+    "revision": "eb1212a9a531504a1b43",
+    "url": "/haden-liu/static/js/2.da9a1fca.chunk.js"
+  },
+  {
+    "revision": "73ab36127f504ad78636e9c40d2d1aab",
+    "url": "/haden-liu/static/js/2.da9a1fca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e8161356437cf74f058",
+    "url": "/haden-liu/static/js/main.73107a6f.chunk.js"
+  },
+  {
+    "revision": "d73027063a319e8b676a",
+    "url": "/haden-liu/static/js/runtime-main.1a79192e.js"
+  },
+  {
+    "revision": "a6aab40b9677f45f6d8fa5c5469d436e",
+    "url": "/haden-liu/static/media/background.a6aab40b.jpg"
+  }
+]);
